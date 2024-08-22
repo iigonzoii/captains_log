759da6b3295b0c826cb4e71761b721a55b06a567
iigonzoii <a.gonzalez418@gmail.com>
2024-08-21 19:02:53 -0500

    fixed tablename

--- app/models/course.py
+++ app/models/course.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Course(db.Model):
-    __tablename__ = 'albums'
+    __tablename__ = 'courses'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
