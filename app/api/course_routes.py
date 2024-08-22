886cf2c6b0186fd600615e32913457d2f5aef242
iigonzoii <a.gonzalez418@gmail.com>
2024-08-22 17:02:22 -0500

    delete route works

--- app/api/course_routes.py
+++ app/api/course_routes.py
@@ -10,12 +10,10 @@ course_routes = Blueprint('courses', __name__)
 @course_routes.route('/', methods=['GET'])
 def all_courses():
     courses = Course.query.all()
-    print("SKEEEE",courses)
     if not courses:
         return {'errors': {'message': 'No existing courses'}}, 404
 
     courses_dict = [ course.to_dict() for course in courses ]
-    print("YEET",courses_dict)
     return {"courses":courses_dict}, 200
 
 #* Get course by id
@@ -36,6 +34,7 @@ def courses_by_user():
     return {"courses":[course.to_dict() for course in courses]}, 200
 
 
+#* Create a new course
 # @album_routes.route('/', methods=['GET', 'POST'])
 # @login_required
 # def create_album():
@@ -59,3 +58,45 @@ def courses_by_user():
 #         return new_album.to_dict(), 201
 
 #     return {'errors': form.errors}, 400
+
+#* Update course by id
+# @album_routes.route('/<int:album_id>/', methods=['PUT'])
+# @login_required
+# def update_album(album_id):
+#     album_update = Album.query.filter(Album.id == album_id).first()
+
+#     if not album_update:
+#         return {'errors': {'message': 'Album not found'}}, 404
+#     if album_update.user_id != current_user.id:
+#         return {'errors': {'message': 'Unauthorized'}}, 401
+
+#     form = UpdateAlbumForm()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+
+#     if form.validate_on_submit():
+#         album_update.band = form.band.data
+#         album_update.title = form.title.data
+#         album_update.cover_image_url = form.cover_image_url.data
+#         album_update.description = form.description.data
+#         album_update.producer = form.producer.data
+#         album_update.genre = form.genre.data
+#         album_update.tags = form.tags.data
+
+#         db.session.commit()
+#         return album_update.to_dict()
+#     return form.errors, 401
+
+
+
+#* Delete course by id
+@course_routes.route('/<int:course_id>', methods=['DELETE'])
+@login_required
+def delete_course(course_id):
+    course = Course.query.get(course_id)
+    if not course or course.owner_id != current_user.id:
+        return {'errors': 'Course not found or unauthorized'}, 404
+
+    db.session.delete(course)
+    db.session.commit()
+
+    return {'message': 'Course deleted successfully'}, 200
