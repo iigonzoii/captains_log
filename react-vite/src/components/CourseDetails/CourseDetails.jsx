f2c034a8d4626d26149aff9f348397627d66d7ae
iigonzoii <a.gonzalez418@gmail.com>
2024-08-30 13:29:10 -0500

    modal css

--- react-vite/src/components/CourseDetails/CourseDetails.jsx
+++ react-vite/src/components/CourseDetails/CourseDetails.jsx
@@ -8,17 +8,6 @@ import ReviewButton from "../ReviewFormModal/ReviewButton";
 import EditReviewModal from "../ReviewFormModal/EditReviewModal"
 import DeleteReviewModal from "../ReviewFormModal/DeleteReviewModal"
 import OpenModalButton from "../OpenModalButton";
-///// todo fetch course
-///// * use params to grab id and effect with thunk
-///// todo fetch experiences
-/////* make experiences model
-/////* make experiences seeders
-/////* update course model to have poi as a string or text area
-///// todo integrate modal for experiences
-/////* import use modal button or make a seperate page like i did with the forms
-// todo apply desktop view with grid template areas
-//* decide on row heights, gaps, for cols and rows
-//* decide on either setting route highlight image as background or banner
 
 
 function CourseDetails() {
@@ -40,21 +29,14 @@ function CourseDetails() {
             .then(() => dispatch(fetchCourse(+course_id)))
             .then(() => setIsLoaded(true));
     }, [dispatch, course_id]);
-    // useEffect(() => {
-    //     //! will probably need to watch experience to rerender after someone messes with it
-    //     dispatch(fetchCourse(course_id))
-    //     //! also will probably need to watch courseId if its buggy
-    // }, [dispatch, course_id])
-
 
     return isLoaded && (
         <div className="cd-container">
+
             <div className="cd-name">{course.name}</div>
+            {/*! once i change logs to a map, change text area to a div and set the overflow for scroll. textarea can only have one child!*/}
             <textarea disabled className="cd-log">{course.log_entry}</textarea>
-
-
             <div className="cd-reviews">
-
                 {reviews.length > 0 ? reviews && reviews.map((review, index) => (
                   <div key={index}>
                         {`UserId-${review.user_id}-${review.review}`}
@@ -83,7 +65,8 @@ function CourseDetails() {
                         <ReviewButton reviews={reviews} course_id={course_id} />
                     )}
                 </div>
-            )}</div>
+            )}
+            </div>
             <div className="cd-poi">{course.poi}</div>
             {/* below images div will be an array at some point being mapped over */}
             <div className="cd-img-container">
